acee8b6b20b65e3518f92384680db2f7933bbd2e
Marco Barraza <marko.barraza@gmail.com>
2019-05-26 18:04:35 -0400

    nav + add swiper

--- js/scripts.js
+++ js/scripts.js
@@ -9,3 +9,26 @@ document.addEventListener('DOMContentLoaded', function () {
 $(document).ready(function () {
     $('.sidenav').sidenav();
 });
+
+
+// SWIPER SLIDER
+
+      $(document).ready(function () {
+            var swiper = new Swiper('.swiper-container', {
+              slidesPerView: 3,
+              spaceBetween: 10,
+              slidesPerGroup: 3,
+              loop: true,
+              loopFillGroupWithBlank: true,
+              pagination: {
+                  el: '.swiper-pagination',
+                  clickable: true,
+              },
+              navigation: {
+                  nextEl: '.swiper-button-next',
+                  prevEl: '.swiper-button-prev',
+              },
+          });
+        });
+    
+  
