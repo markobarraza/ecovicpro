67b9d2cc54191ab5c9e4fab5a2b7f255566713fd
Marcos <marcos@iMac-de-Marcos.local>
2019-06-03 18:11:04 -0400

    add icons iconmoon front + tooltips

--- js/scripts.js
+++ js/scripts.js
@@ -13,22 +13,34 @@ $(document).ready(function () {
 
 // SWIPER SLIDER
 
-      $(document).ready(function () {
-            var swiper = new Swiper('.swiper-container', {
-              slidesPerView: 3,
-              spaceBetween: 1,
-              slidesPerGroup: 3,
-              loop: false,
-              loopFillGroupWithBlank: true,
-              pagination: {
-                  el: '.swiper-pagination',
-                  clickable: true,
-              },
-              navigation: {
-                  nextEl: '.icon-keyboard_arrow_right',
-                  prevEl: '.icon-keyboard_arrow_left',
-              },
-          });
-        });
+$(document).ready(function () {
+    var swiper = new Swiper('.swiper-container', {
+        slidesPerView: 3,
+        spaceBetween: 1,
+        slidesPerGroup: 3,
+        loop: false,
+        loopFillGroupWithBlank: true,
+        pagination: {
+            el: '.swiper-pagination',
+            clickable: true,
+        },
+        navigation: {
+            nextEl: '.icon-keyboard_arrow_right',
+            prevEl: '.icon-keyboard_arrow_left',
+        },
+    });
+});
+
+// TOOLTIPS
+
+document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('.tooltipped');
+    var instances = M.Tooltip.init(elems, options);
+  });
+
+  // Or with jQuery
+
+  $(document).ready(function(){
+    $('.tooltipped').tooltip();
+  });
     
-  
